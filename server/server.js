bc13c00344fe0754dd3a651b441989a76699f4bc
Akshay Kumar <124715918+akshay030@users.noreply.github.com>
2025-06-04 13:13:01 +0530

    PORT

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.use('/api/admin',adminRouter)
 app.use('/api/blog',blogRouter)
 
 
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT ||3000 ;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
